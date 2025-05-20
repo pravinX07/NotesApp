b815d8889431777e027d5017b8d40bb7558d111f
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-20 17:20:21 +0530

    initialize frontend and build server and db connection

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './connection.js'
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+
+const PORT = process.env.PORT || 8000
+connectDb()
+app.get('/',(req,res)=>{
+    res.status(200).json({msg:"Server is running "})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+    
+})
