36bbe5b3cf3549d24ee1609bf0e609b54d9d9d9c
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-26 23:29:31 +0530

    create login route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,28 @@
-import express from 'express'
+// controllers/authController.js
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
-const router = express.Rou
+export const signup = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: "Email already exists" });
+
+    const user = await User.create({ userName, email, password });
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // Save hashed refresh token
+    const hashed = await bcrypt.hash(refreshToken, 10);
+    user.refreshTokenHash = hashed;
+    await user.save();
+
+    res.status(201).json({ accessToken, refreshToken, user: { userName, email } });
+  } catch (err) {
+    res.status(500).json({ message: "Signup failed", error: err.message });
+  }
+};
+
+
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './connection.js'
+import { signup } from './controllers/user.js'
+import User from './models/user.js'
 
 const app = express()
 dotenv.config()
@@ -8,10 +10,11 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 8000
 connectDb()
-app.get('/',(req,res)=>{
-    res.status(200).json({msg:"Server is running "})
-})
 
+app.get("/",async(req,res)=>{
+    res.status(200).json({message:"Server is running Successfully"})
+})
+app.post("/signup",signup)
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
